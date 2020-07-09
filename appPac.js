30a56e2e4132e52b8db07f7a712528637cd73615
Christiano0407 <placebo0704@gmail.com>
2020-07-09 07:19:17 -0500

    pac2

--- appPac.js
+++ appPac.js
@@ -33,4 +33,34 @@ document.addEventListener(`DOMContentLoaded`, () => {
         1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
         1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
     ]
+
+     // 0 - pac-dots
+  // 1 - wall
+  // 2 - ghost-lair
+  // 3 - power-pellet
+  // 4 - empty
+
+  const squares = []
+
+  //create your board
+  function createBoard() {
+    for (let i = 0; i < layout.length; i++) {
+      const square = document.createElement('div')
+      grid.appendChild(square)
+      squares.push(square)
+
+      //add layout to the board
+      if(layout[i] === 0) {
+        squares[i].classList.add('pac-dot')
+      } else if (layout[i] === 1) {
+        squares[i].classList.add('wall')
+      } else if (layout[i] === 2) {
+        squares[i].classList.add('ghost-lair')
+      } else if (layout[i] === 3) {
+        squares[i].classList.add('power-pellet')
+      }
+    }
+  }
+  createBoard()
+
 })
